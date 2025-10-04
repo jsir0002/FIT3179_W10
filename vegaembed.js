27571aa3419913f71c72dbc1bce6fa9256d236fd
jsir0002 <139840299+jsir0002@users.noreply.github.com>
2025-10-04 14:25:51 +1000

    Initial files commit

---
+++ vegaembed.js
@@ -0,0 +1,7 @@
+var vg_1 = "avgtemp_aus.json";
+var vg_2 = "totalfirearea.json";
+
+vegaEmbed("#avgtemp_aus", vg_1).then(function(result) {
+}).catch(console.error);
+vegaEmbed("#totalfirearea", vg_2).then(function(result) {
+}).catch(console.error);
